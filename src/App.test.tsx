835f980f7b82fde2dddd514af6deef1ab972773d
sandyatech <sandhya.mskgmail.com>
2023-03-18 12:38:09 +0000

    included tests for 500 and 418 error status code

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,15 +1,45 @@
-// import react-testing methods
-import { getByText, render, screen } from '@testing-library/react'
+﻿// import react-testing methods
+import { render, screen } from '@testing-library/react'
 import { rest } from 'msw'
 import { setupServer } from 'msw/node'
 import '@testing-library/jest-dom'
 import App from './App'
+import { FETCH_ERROR_500, FETCH_ERROR_418 } from './constants/error_type'
 
 
+
+const server = setupServer(
+    rest.get(`https://swapi.dev/api/people/1/`, (req, res, ctx) => {
+        return res(ctx.json({ name: 'Luke Skywalker' }))
+    }),
+)
+
+beforeAll(() => server.listen())
+afterEach(() => server.resetHandlers())
+afterAll(() => server.close())
 describe("Sw App", () => {
     test('loads and displays character', async () => {
         render(<App />)
-        const name = await screen.findAllByText(/.*Luke Skywalker.*/i);
-        expect(name[0]).toBeInTheDocument();
+        expect(await screen.findByText(/Luke Skywalker/i)).toBeInTheDocument();
+    })
+
+    test('Checking of Status Error code 500', async () => {
+        server.use(
+            rest.get(`https://swapi.dev/api/people/1/`, (req, res, ctx) => {
+                return res(ctx.status(500))
+            })
+        );
+        render(<App />)
+        expect(await screen.findByText(FETCH_ERROR_500)).toBeInTheDocument();
+    })
+
+    test('Checking of Status Error code 418', async () => {
+        server.use(
+            rest.get(`https://swapi.dev/api/people/1/`, (req, res, ctx) => {
+                return res(ctx.status(418))
+            })
+        );
+        render(<App />)
+        expect(await screen.findByText(FETCH_ERROR_418)).toBeInTheDocument();
     })
 });
--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,21 @@ import React, { useState, useEffect } from 'react';
 import { SwCharName } from './sw_char_name';
 import SwCharContainer from './components/SwCharContainer';
 import './App.css';
+import { FETCH_ERROR_500, FETCH_ERROR_418 } from './constants/error_type'
 const App: React.FC = () => {
     const [charNumber, setCharNumber] = useState<number>(1);
     const [swChar, setSwChar] = useState<string>('');
     useEffect(() => {
         const getName = async (dataItem: number) => {
             const apiResponse = await fetch(`https://swapi.dev/api/people/${dataItem}/`);
-            const json = await apiResponse.json();
-            setSwChar(json.name);
+            switch (apiResponse.status) {
+                case 200: const json = await apiResponse.json();
+                    setSwChar(json.name); break
+                case 500: setSwChar(FETCH_ERROR_500); break
+                case 418: setSwChar(FETCH_ERROR_418); break
+            }
+            
+
         }
         getName(charNumber);
     }, [charNumber]);
